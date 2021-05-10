3e6f0decee22d20b734401932e38136575d17251
LuccasTxr <luccas.teixeira@gmail.com>
2021-05-10 17:49:29 -0300

    BD

--- infoBD.sql
+++ infoBD.sql
@@ -6,37 +6,38 @@ create database infoBD;
 show databases;
 
 use infoBD;
-create table servico(
-	id_servico int not null auto_increment,
-	nome varchar(30) not null unique,
-	descricao_s varchar(50)not null,
-	valor float(15) not null,
-	primary key(id_servico)
+create table produto(
+	id_produto INT NOT NULL auto_increment,
+	nome VARCHAR(30) NOT NULL UNIQUE,
+	descricao_p VARCHAR(50) NOT NULL,
+	valor FLOAT(15) NOT NULL,
+	primary key(id_produto)
 );
 
-create table produto(
-	id_produto int not null auto_increment,
-	nome varchar(30) not null unique,
-	descricao_p varchar(50) not null,
-	valor float(15) not null,
-	primary key(id_produto)
+create table servico(
+	id_servico INT NOT NULL auto_increment,
+	nome VARCHAR(30) NOT NULL,
+	descricao_s VARCHAR(50)NOT NULL,
+	valor FLOAT(15) NOT NULL,
+	primary key(id_servico)
 );
 
 create table pedido(
 	id_pedido int not null auto_increment,
-	id_servico int not null,
 	id_produto int not null,
+	id_servico int not null,
 	descricao_pedido varchar(150) not null,
 	data varchar(30) not null,
 	hora varchar(30) not null,
 	status varchar(30) not null,
 	valor float(15) not null,
 	primary key(id_pedido),
-	constraint fk_p_s foreing key (id_servico)
-	references servico(id_servico),
-	constraint fk_p_p foreing key (id_produto)
-	references produto(id_produto)
+	constraint fk_i_pr foreign key(id_produto)
+    references produto(id_produto),
+	constraint fk_i_s foreign key (id_servico)
+	references servico(id_servico)
 );
+
 
 insert into servico values
 (1,"Formatação","Limpesa do sistema e reinstalação.","70,00$"),
@@ -53,13 +54,13 @@ insert into produto values
 (5,"Placa de Vídeo","GTX 1060Ti","1080,00$");
 
 insert into pedido values
-(1,2,4,"04/05/2021","08:23","Em Andamento","440,00$"),
-(2,4,0,"04/05/2021","10:55","Em Andamento","50,00$"),
-(3,5,0,"04/05/2021","13:30","Entregue","25,00$"),
-(4,2,0,"04/05/2021","13:35","Aguardando","90,00$"),
-(5,2,0,"04/05/2021","15:47","Aguardando","90,00$");
+(1,1,1,"revisão","04/05/2021","08:23","Em Andamento","440,00$"),
+(2,2,2,"revisão","04/05/2021","10:55","Em Andamento","50,00$"),
+(3,3,3,"revisão","04/05/2021","13:30","Entregue","25,00$"),
+(4,4,4,"revisão","04/05/2021","13:35","Aguardando","90,00$"),
+(5,5,5,"revisão","04/05/2021","15:47","Aguardando","90,00$");
 
-select * from servico;
 select * from produto;
+select * from servico;
 select * from pedido;
 show tables;
